5ee213d85567fcca7369f254ad7b4f22507ce904
Michael Pond <murraymax_17@hotmail.com>
2023-10-18 11:21:39 -0600

    added read me and output files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,36 @@
 import os
 import csv
 
-budget_data_csv = os.path.join('..', 'resources', 'budget_data.csv')
+csvpath = os.path.join('PyBank','resources', 'budget_data.csv')
 
-with open(budget_data_csv) as csvfile:
+
+with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
-
+    
     csv_header = next(csvreader)
     print(f'CSV Header: {csv_header}')
 
-    for row in csvreader:
-        print(row)
+    row_count = sum(1 for row in csvreader)
+    Total_Months = row_count
+    print(Total_Months)
+
+output_path = os.path.join('PyBank','Analysis','Output')
+
+with open(output_path, 'w') as csvfile:
+
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    csvwriter.writerow(['Financial Analysis'])
+    csvwriter.writerow(['------------------------'])
+    csvwriter.writerow(['Total Months: '])
+    csvwriter.writerow(['Total: '])
+    csvwriter.writerow(['Average Change: '])
+    csvwriter.writerow(['Greatest Increase in Profits: '])
+    csvwriter.writerow(['Greatest Decrease in Profits: '])
 
 
+
+    
+    
+    
--- PyPoll/analysis/main.py
+++ PyPoll/analysis/main.py
@@ -1,7 +1,8 @@
 import os
 import csv
 
-election_data_csv = os.path.join('..','resources', 'election_data.csv')
+
+election_data_csv = os.path.join('..','Resources', 'election_data.csv')
 
 with open(election_data_csv) as csvfile:
     
