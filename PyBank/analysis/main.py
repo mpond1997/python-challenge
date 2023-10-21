e8091c601200d7d4fad75e372f38d6f33603223d
Michael Pond <murraymax_17@hotmail.com>
2023-10-21 09:58:26 -0600

    Finalizing code

--- PyBank/analysis/main.py
+++ PyBank/analysis/main.py
@@ -1,17 +1,72 @@
 import os
 import csv
-
-csvpath = os.path.join('..','resources', 'budget_data.csv')
-
+# File Location
+csvpath = os.path.join('PyBank','resources', 'budget_data.csv')
+# Defining Values
+total = 0
+value = 0
+avg_change = 0
+date = []
+profits = []
+# Opening csvfile and printing header
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
-    
-
+    # Header row
     csv_header = next(csvreader)
-    print(f'CSV Header: {csv_header}')
-
+    
+    first_row = next(csvreader)
+    total += int(first_row[1])
+    value = int(first_row[1])
     for row in csvreader:
-        print(row)
+        date.append(row[0])
+        
+        change = int(row[1])-value
+        profits.append(change)
+        value - int(row[1])
+# Calculating rows for months
+        total_months = sum(1 for row in csvreader)
+# Calculating sum of Profit/Losses for Total $        
+        total = total +int(row[1])
 
 
+
+# Calculating Greatest increase and Decrease
+    greatest_increase = max(profits)
+    greatest_index = profits.index(greatest_increase)
+    greatest_date = date[greatest_index]
+
+    greatest_decrease = min(profits)
+    worst_index = profits.index(greatest_decrease)
+    worst_date = date[worst_index]
+
+    # Calculating average change
+    avg_change =sum(profits)/len(profits)
+
+
+#Printing values
+print("Financial Analysis")
+print("---------------------")
+print(f"Total Months: {str(total_months)}")
+print(f"Total: ${str(total)}")
+print(f"Average Change: ${str(round(avg_change,2))}")
+print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+
+
+# Writing out info to file (Output)
+output = open('PyBank/analysis/Output','w')
+
+
+line1 = 'Financial Analysis'
+line2 = '------------------------'
+line3 = str(f"Total Months: {str(total_months)}")
+line4 = str(f'Total: ${str(total)}')
+line5 = str(f'Average Change: ${str(round(avg_change,2))}')
+line6 = str(f'Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})')
+line7 = str(f'Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})')
+output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
+
+
+    
+    
+    
--- PyPoll/analysis/main.py
+++ PyPoll/analysis/main.py
@@ -1,11 +1,63 @@
 import os
 import csv
+#Define Values
+candidate = []
+num_votes = []
+vote_percent = []
+total_votes = 0
 
 
-election_data_csv = os.path.join('..','Resources', 'election_data.csv')
+election_data_csv = os.path.join('PyPoll','Resources', 'election_data.csv')
 
 with open(election_data_csv) as csvfile:
-    
     csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
 
-    print(csvreader)
+    for row in csvreader:
+        total_votes += 1
+
+        if row[2] not in candidate:
+            candidate.append(row[2])
+            index = candidate.index(row[2])
+            num_votes.append(1)
+        else:
+            index = candidate.index(row[2])
+            num_votes[index] += 1
+    
+     
+    for votes in num_votes:
+        percentage = (votes/total_votes) * 100
+        percentage = round(percentage)
+        percentage = "%.3f%%" % percentage
+        vote_percent.append(percentage)
+    
+    # Find the winning candidate
+    winner = max(num_votes)
+    index = num_votes.index(winner)
+    winning_candidate = candidate[index]
+
+#Printing results
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {str(total_votes)}")
+print("--------------------------")
+for i in range(len(candidate)):
+    print(f"{candidate[i]}: {str(vote_percent[i])} ({str(num_votes[i])})")
+print("--------------------------")
+print(f"Winner: {winning_candidate}")
+print("--------------------------")
+
+# Printing in output file
+output = open("output", "w")
+line1 = "Election Results"
+line2 = "--------------------------"
+line3 = str(f"Total Votes: {str(total_votes)}")
+line4 = str("--------------------------")
+output.write('{}\n{}\n{}\n{}\n'.format(line1, line2, line3, line4))
+for i in range(len(candidate)):
+    line = str(f"{candidate[i]}: {str(vote_percent[i])} ({str(num_votes[i])})")
+    output.write('{}\n'.format(line))
+line5 = "--------------------------"
+line6 = str(f"Winner: {winning_candidate}")
+line7 = "--------------------------"
+output.write('{}\n{}\n{}\n'.format(line5, line6, line7))   
